ff27d254af4484f9bd69189fe9c2e43f8e978f40
Diana Zhou <Diana@Dianas-MacBook-Pro.local>
2015-07-11 22:28:08 -0700

    added first slide

--- Escalapp/Escalapp/ViewController.m
+++ Escalapp/Escalapp/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [label setTransform:CGAffineTransformMakeRotation(-M_PI / 2)];
 }
 
 - (void)didReceiveMemoryWarning {
