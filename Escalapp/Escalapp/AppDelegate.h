3b4c8e1ffea65476dce1c746f600a3eabe83ffc0
Diana Zhou <Diana@Dianas-MacBook-Pro.local>
2015-07-11 19:41:28 -0700

    files

--- Escalapp/Escalapp/AppDelegate.h
+++ Escalapp/Escalapp/AppDelegate.h
@@ -11,7 +11,8 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property NSString *playerName;
+@property BOOL prefersMale;
 
 @end
 
